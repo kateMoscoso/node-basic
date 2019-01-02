7a2a1fedccf781a3b31a200962baf0b72b85e1c1
kateMoscoso <katherin.mleon@alumnos.pm.es>
2019-01-03 00:00:03 +0100

    move file

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const logger = require("morgan");
+const bodyParser = require("body-parser");
+const app = express();
+
+app.use(logger("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+require("./routes")(app);
+
+app.get("*", (req, res) => {
+    res.send("Hola, soy Express!");
+});
+
+app.listen(3000, () => {
+    console.log("Express ha iniciado correctamente!");
+});
