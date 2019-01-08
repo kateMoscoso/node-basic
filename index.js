115adc27f25285d4cd628330e079aaba404dd0a8
kateMoscoso <katherin.mleon@alumnos.pm.es>
2019-01-08 20:47:28 +0100

    simple respurces

--- index.js
+++ index.js
@@ -1,13 +1,43 @@
 const express = require("express");
 const logger = require("morgan");
 const bodyParser = require("body-parser");
+const PORT = process.env.PORT || 3000;
+
 const app = express();
+
+const mongo = require("./db/connect");
 
 app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-require("./routes")(app);
+require("./routes/views")(app);
+require("./routes/api")(app);
+
+require("./routes/special")(app);
+
+
+async function initMongo() {
+    const db = await mongo.connect();
+    if (db) {
+        initExpress();
+    }
+}
+
+function initExpress() {
+    app.listen(PORT, () => {
+        console.log(`Express ready on port ${PORT}`);
+        process.on('SIGINT', closeApp);
+        process.on('SIGTERM', closeApp);
+    });
+}
+
+function closeApp() {
+    mongoose.disconnect()
+        .then(() => process.exit(0));
+
+}
+
 
 app.get("*", (req, res) => {
     res.send("Hola, soy Express!");
