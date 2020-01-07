53720571eb454019caf02abd30f5e06bf45fd0de
kateMoscoso <katiemscs@gmail.com>
2020-01-07 19:40:15 +0100

    test jest

--- server/index.js
+++ server/index.js
@@ -1,11 +1,11 @@
-const http = require('http')
-const fs = require('fs')
+const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-	fs.readFile("./server/my_page2.html", (err, data) => {
-		if(err){console.error(err); return;}
-		res.end(data)
-	})
-})
+  fs.readFile('./server/my_page2.html', (err, data) => {
+    if (err) { console.error(err); return; }
+    res.end(data);
+  });
+});
 
-server.listen(3000)
+server.listen(3000);
--- test/math.test.js
+++ test/math.test.js
@@ -1,37 +1,37 @@
 const assert = require('assert');
-const math = require("../math/math");
+const math = require('../math/math');
 
-describe('Math module', function() {
-    describe('@sum', function() {
-        it('should return 3 when params are 1 and 2', function() {
-            assert.equal(math.sum(1, 2), 3);
-        });
-        it('should return -1 when params are 1 and -2', function() {
-            assert.equal(math.sum(1, -2), -1);
-        });
+describe('Math module', () => {
+  describe('@sum', () => {
+    it('should return 3 when params are 1 and 2', () => {
+      assert.equal(math.sum(1, 2), 3);
     });
-    describe('@diff', function() {
-        it('should return -1 when params are 1 and 2', function() {
-            assert.equal(math.diff(1, 2), -1);
-        });
-        it('should return 4 when params are 2 and -2', function() {
-            assert.equal(math.diff(2, -2), 4);
-        });
+    it('should return -1 when params are 1 and -2', () => {
+      assert.equal(math.sum(1, -2), -1);
     });
-    describe('@product', function() {
-        it('should return 5 when params are 2 and 2.5', function() {
-            assert.equal(math.prod(2, 2.5), 5);
-        });
-        it('should return 0 when params are 100 and 0', function() {
-            assert.equal(math.prod(100, 0), 0);
-        });
+  });
+  describe('@diff', () => {
+    it('should return -1 when params are 1 and 2', () => {
+      assert.equal(math.diff(1, 2), -1);
     });
-    describe('@div', function() {
-        it('should return 2 when params are 6 and 3', function() {
-            assert.equal(math.div(6, 3), 2);
-        });
-        it('should return Infinity when params are 1 and 0', function() {
-            assert.equal(math.div(1, 0), Infinity);
-        });
+    it('should return 4 when params are 2 and -2', () => {
+      assert.equal(math.diff(2, -2), 4);
     });
+  });
+  describe('@product', () => {
+    it('should return 5 when params are 2 and 2.5', () => {
+      assert.equal(math.prod(2, 2.5), 5);
+    });
+    it('should return 0 when params are 100 and 0', () => {
+      assert.equal(math.prod(100, 0), 0);
+    });
+  });
+  describe('@div', () => {
+    it('should return 2 when params are 6 and 3', () => {
+      assert.equal(math.div(6, 3), 2);
+    });
+    it('should return Infinity when params are 1 and 0', () => {
+      assert.equal(math.div(1, 0), Infinity);
+    });
+  });
 });
