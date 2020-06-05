a102c1858f32ad978a7d22ba2a71af202ff4372c
kateMoscoso <katiemscs@gmail.com>
2020-06-05 21:57:13 +0200

    typescript

---
+++ media-player/assets-ts/MediaPlayer.ts
@@ -0,0 +1,53 @@
+class MediaPlayer {
+  media: HTMLMediaElement
+  plugins: Array <any>
+  constructor(config) {
+    this.media = config.el;
+    this.plugins = config.plugins || [];
+    this.initPlugins();
+  }
+  private initPlugins() {
+    const player = {
+      play: () => this.play(),
+      pause: () => this.pause(),
+      media: this.media,
+      get muted() {
+        return this.media.muted;
+      },
+      set muted(value) {
+        this.media.muted = value;
+      },
+    };
+    this.plugins.forEach(plugin => {
+      plugin.run(this);
+    });
+  }
+  play() {
+    this.media.play();
+  }
+  pause() {
+    this.media.pause();
+  }
+  togglePlay() {
+    if (this.media.paused) {
+      this.play();
+    }
+    else {
+      this.pause();
+    }
+  }
+  mute() {
+    this.media.muted = true;
+  }
+  unmute() {
+    this.media.muted = false;
+  }
+}
+
+
+
+
+
+
+
+export default MediaPlayer;
