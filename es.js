c181cceb6b2b794c540e0f9c208b7a01cb306d34
kateMoscoso <katiemscs@gmail.com>
2020-06-05 19:23:30 +0200

    examples

---
+++ es.js
@@ -0,0 +1,6 @@
+const myString = 'hello'
+console.log(myString.padStart(8, 'hi ')) // hi hello
+console.log(myString.padEnd(12, ' _____')) //hello ______
+console.log('food'.padEnd(12, ' ______')) // food ______
+
+33
