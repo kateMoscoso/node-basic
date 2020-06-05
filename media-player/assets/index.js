c073cee821c0436d5a55247f1b241af1146bc77b
kateMoscoso <katiemscs@gmail.com>
2020-06-05 21:30:16 +0200

    service worker

--- media-player/assets/index.js
+++ media-player/assets/index.js
@@ -19,3 +19,10 @@ muteButton.onclick = () => {
     player.mute();
   }
 };
+
+if('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/sw.js').catch(error => {
+    console.log(error.message)
+  })
+
+}
