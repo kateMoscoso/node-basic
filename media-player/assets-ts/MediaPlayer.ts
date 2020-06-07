798976e02acfe8ca27512daee916e9a4740282a1
kateMoscoso <katiemscs@gmail.com>
2020-06-07 13:48:20 +0200

    ad examples

--- media-player/assets-ts/MediaPlayer.ts
+++ media-player/assets-ts/MediaPlayer.ts
@@ -1,53 +1,51 @@
 class MediaPlayer {
-  media: HTMLMediaElement
-  plugins: Array <any>
+  media: HTMLMediaElement;
+  plugins: Array<any>;
+  container: HTMLElement;
+
   constructor(config) {
     this.media = config.el;
     this.plugins = config.plugins || [];
+    this.initPlayer();
     this.initPlugins();
   }
+
+  initPlayer() {
+    this.container = document.createElement('div');
+    this.container.style.position = 'relative';
+    this.media.parentNode.insertBefore(this.container, this.media);
+    this.container.appendChild(this.media);
+  }
+
   private initPlugins() {
-    const player = {
-      play: () => this.play(),
-      pause: () => this.pause(),
-      media: this.media,
-      get muted() {
-        return this.media.muted;
-      },
-      set muted(value) {
-        this.media.muted = value;
-      },
-    };
     this.plugins.forEach(plugin => {
       plugin.run(this);
     });
   }
+
   play() {
     this.media.play();
   }
+
   pause() {
     this.media.pause();
   }
+
   togglePlay() {
     if (this.media.paused) {
       this.play();
-    }
-    else {
+    } else {
       this.pause();
     }
   }
+
   mute() {
     this.media.muted = true;
   }
+
   unmute() {
     this.media.muted = false;
   }
 }
-
-
-
-
-
-
 
 export default MediaPlayer;
--- media-player/assets-ts/index.ts
+++ media-player/assets-ts/index.ts
@@ -1,11 +1,12 @@
 import MediaPlayer from './MediaPlayer';
 import AutoPlay from './plugins/AutoPlay';
 import AutoPause from './plugins/AutoPause';
+import Ads from './plugins/Ads';
 
 const video = document.querySelector('video');
 const player = new MediaPlayer({
   el: video,
-  plugins: [new AutoPlay(), new AutoPause()],
+  plugins: [new AutoPlay(), new AutoPause(), new Ads()],
 });
 
 const playButton: HTMLElement = document.querySelector('#playButton');
@@ -20,9 +21,8 @@ muteButton.onclick = () => {
   }
 };
 
-if('serviceWorker' in navigator) {
+if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('/sw.js').catch(error => {
-    console.log(error.message)
-  })
-
+    console.log(error.message);
+  });
 }
