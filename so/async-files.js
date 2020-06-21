67f8c36775031a9122bb4dca44ba0c01a785f910
kateMoscoso <katiemscs@gmail.com>
2020-06-21 20:07:52 +0200

    so examples

---
+++ so/async-files.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+
+const file = process.argv[2];
+
+if (!file) {
+  throw new Error("Debes indicar el archivo que quieres leer");
+}
+
+const content = fs.readFile(file, function(err, content) {
+  if (err) {
+    return console.log(err);
+  }
+
+  const lines = content.toString().split("\n").length;
+  console.log(lines);
+});
+
+// no necesita errror porque el error se maneja desde el callback
---
+++ so/sync-files.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+
+try {
+  const file = process.argv[2];
+
+  const content = fs.readFileSync(file).toString();
+
+  const lines = content.split("\n").length;
+  console.log(lines);
+} catch (err) {
+  console.log(err);
+}
+
+//node sync-files.js name_file
